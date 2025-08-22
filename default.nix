634ea622842e4c8c9e4399ea8ccdcac0a07beabe
MattheDev53 <matthew.c.albrecht@gmail.com>
2025-08-22 18:41:42 -0500

    initial

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ pkgs, appimageTools, makeDesktopItem, ... }:
+let
+pname = "beeper";
+version = "4.1.135";
+src = pkgs.fetchurl {
+  url = "https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop";
+  hash = "sha256-bp0RGU689A8kgphNgJJnlbQBh1fAubwWUvM9StzLwB4=";
+};
+
+appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
+  pkgs = pkgs;
+  extraInstallCommands = ''
+          cp -r ${appimageContents}/usr/share/icons $out/share
+
+          # unless linked, the binary is placed in $out/bin/cursor-someVersion
+          # ln -s $out/bin/${pname}-${version} $out/bin/${pname}
+        '';
+  desktopItems = [
+    (makeDesktopItem {
+      name = "beeper";
+      exec = "beeper %U";
+      icon = "beeper";
+      desktopName = "Beeper";
+      comment = "Beeper Beeper Desktop";
+      categories = [ "Internet" ];
+    })
+  ];
+}
